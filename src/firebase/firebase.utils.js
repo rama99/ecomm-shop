bc8e6f9d5d74dc2df0674cd7362ce6a51a021924
rama99 <rama.kishore@gmail.com>
2019-11-21 22:38:27 +0530

    added create user

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,34 @@ const config = {
     appId: "1:962326443842:web:958d14a357d7b322cd3adb"
 }
 
+export const createUserProfileDocument = async (userAuth , additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const { displayName , email } = userAuth;
+        const createdAt = new  Date();
+    
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }
+        catch(err) {
+
+        }
+
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const  auth = firebase.auth();
