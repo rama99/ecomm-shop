10f4e0c77df4a658bef64fcfe401584cdf8b8fab
rama99 <rama.kishore@gmail.com>
2019-11-23 12:37:07 +0530

    Added redirect functionality to sign in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch , Route } from 'react-router-dom';
+import { Switch , Route , Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 
@@ -62,7 +62,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={() => this.props.currentUser ?  (<Redirect to='/' />) : <SignInAndSignUpPage/>} />
         </Switch>
       </div>
     );
@@ -73,4 +73,8 @@ class App extends React.Component {
     setCurrentUser: user => dispatch(setCurrentUser(user))
   })
 
-export default connect(null , mapDispatchToProps)(App);
+  const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser
+  })
+
+export default connect(mapStateToProps , mapDispatchToProps)(App);
