2700aeeab4971d0a23850de65398fcf0a06f2bba
rama99 <rama.kishore@gmail.com>
2019-11-24 19:14:13 +0530

    modified to use cart reselect

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,6 +4,7 @@ import './cart-dropdown.styles.scss';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 const CartDropDown = ({cartItems}) => (
 
@@ -22,8 +23,8 @@ const CartDropDown = ({cartItems}) => (
 
 )
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems : selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropDown);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,27 +9,32 @@ import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
-const Header = ({currentUser , hidden}) => (
-    <div className='header'>
-        <Link className='logo-container' to='/'>
-            <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>SHOP</Link>
-            <Link className='option' to='/contact'>CONTACT</Link>
-            {
-                currentUser ? 
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                : 
-                <Link className='option' to='/signin'>SIGN IN</Link>
-            }
-            <CartIcon/>
-        </div>
-        {
-          hidden ?  null : <CartDropDown/>
-        }        
-    </div>    
-)
+const Header = ({currentUser , hidden}) => { 
+
+    console.log(`HEADER COMPONENT`);
+    return (
+                <div className='header'>
+                    <Link className='logo-container' to='/'>
+                        <Logo className='logo' />
+                    </Link>
+                    <div className='options'>
+                        <Link className='option' to='/shop'>SHOP</Link>
+                        <Link className='option' to='/contact'>CONTACT</Link>
+                        {
+                            currentUser ? 
+                            <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                            : 
+                            <Link className='option' to='/signin'>SIGN IN</Link>
+                        }
+                        <CartIcon/>
+                    </div>
+                    {
+                    hidden ?  null : <CartDropDown/>
+                    }        
+                </div>    
+            )
+
+}
 
 const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
     currentUser,
